3c195ed66d5e680c776da02cd6610a8efb6d788a
Endeamour <aknyshov@fablab.pro>
2022-07-18 21:25:22 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.red),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Duhast'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            'Duhast Vyacheslavovich',
+            style: TextStyle(
+                fontSize: 20, color: Colors.red, fontFamily: 'Roboto'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print('AAAAAA');
+            },
+            child: Text('Press')),
+        backgroundColor: Colors.cyanAccent,
+      ),
+    );
+  }
+}
