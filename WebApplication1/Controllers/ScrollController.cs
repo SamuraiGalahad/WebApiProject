966d03bb4b72cf626cbd07adea4daf8995d901a2
SamuraiGalahad <nikvik2003@gmail.com>
2022-07-27 19:34:24 +0300

    New

--- WebApplication1/Controllers/ScrollController.cs
+++ WebApplication1/Controllers/ScrollController.cs
@@ -4,19 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Models;
+using System.Web;
 
 namespace WebApplication1.Controllers
 {
     [Route("api/[controller]")]
     public class ScrollController : Controller
     {
-        List<PetClass> ex_data = new List<PetClass> { new PetClass("Name1", 10, "89163386615", "Dog") };
-            
+        List<PetClass> ex_data = new List<PetClass> { new PetClass("Name1", 10, "89163386615", "Dog", 0) };
+
+        /// <summary>
+        /// Это метод Get.
+        /// </summary>
+        /// <returns></returns>
+
         [HttpGet]
         public IEnumerable<PetClass> Get() {
             return ex_data;
         }
+
         
+        /// <summary>
+        /// ДОбавление в бд животоного.
+        /// </summary>
+        [HttpPost("{str}")]
+        public void PostNewPet(string str) { 
+
+        }
+
         //public IActionResult Index()
         //{
         //    return View();
--- WebApplication1/Models/PetClass.cs
+++ WebApplication1/Models/PetClass.cs
@@ -11,12 +11,14 @@ namespace WebApplication1.Models
         private int _age;
         private PhoneClass _phone;
         private string _animal_type;
+        private int _gen;
 
-        public PetClass(string name, int age, string phone, string type) {
-            _name = name;
-            _age = age;
+        public PetClass(string name, int age, string phone, string type, int gen) {
+            Name = name;
+            Age = age;
             _phone = PhoneClass.Parse(phone);
             _animal_type = type;
+            _gen = gen;
         }
 
         public string Name { get => _name; 
@@ -47,13 +49,32 @@ namespace WebApplication1.Models
             } 
         }
 
+        public int Gen { get => _gen;
+            set {
+                if (_gen != 0 && _gen != 1)
+                {
+                    throw new ArgumentException("False gender.");
+                }
+                _gen = value;
+            }
+        }
+
         // Лучше сделать отдельный класс с проверкой.
         public string AnimalType { get => _animal_type; 
             set => _animal_type = value; }
 
+        //public static PetClass Parse(string pet_string) {
+        //    List<string> splited = pet_string.Split().ToList();
+        //    if (splited.Count != 5)
+        //    {
+        //        throw new ArgumentException("Bad data.");
+        //    }
+            
+        //}
+
         public override string ToString()
         {
-            return $"{Name};{AnimalType};{Age};{Phone}";
+            return $"{Name};{AnimalType};{Age};{Phone};{Gen}";
         }
     }
 }
